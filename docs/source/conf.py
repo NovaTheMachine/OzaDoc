09acf62639049468be74d28f410899ebb7d63665
QUENUM GERALD <61545637+NovaTheMachine@users.noreply.github.com>
2022-11-07 12:34:25 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Ozalentour'
+copyright = '2022, NovaTheMachine'
+author = 'NovaTheMachine'
 
 release = '0.1'
 version = '0.1.0'
